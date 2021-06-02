0e26e487bd1dd2513775d2f1ee3882180f486ef9
Алексей <alekseynuzdin@gmail.com>
2021-06-02 17:20:08 +0700

    finish wf

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/index.js'
+  },
+  devServer: {
+    overlay: true,
+    open: false,
+    contentBase: './dist'
+  },  
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
+};
